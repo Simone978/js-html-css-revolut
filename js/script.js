2049b9f9934911815483b5b193355d598ac09cf4
Simone <simonelega.sl@gmail.com>
2020-01-20 20:38:10 +0100

    Terminato stile e hover, aggiunto effetto jquery su arrow down/up

--- js/script.js
+++ js/script.js
@@ -5,5 +5,15 @@ $(document).ready(function(){
     $(this).children('ul').toggleClass('hidden_show');
   });
 
+  $('.it').mouseenter(function(e){
+    e.preventDefault();
+    $(this).children().children().children().eq(1).removeClass('hidden_i');
+    $(this).children().children().children().eq(0).addClass('hidden_i');
+  })
 
+  $('.it').mouseleave(function(e){
+    e.preventDefault();
+    $(this).children().children().children().eq(0).removeClass('hidden_i');
+    $(this).children().children().children().eq(1).addClass('hidden_i');
+  })
 });
